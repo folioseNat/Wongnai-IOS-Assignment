80c8721f2d5c527a065f980a080becc13e1ab3af
Natthawut.K <foliose.k.natthawut@gmail.com>
2020-08-16 12:31:51 +0700

    initial mvvm structure

---
+++ Wongnai-IOS-Assignment/Models/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  Wongnai-IOS-Assignment
+//
+//  Created by Natthawut Kaeoaubon on 16/8/2563 BE.
+//  Copyright © 2563 Natthawut Kaeoaubon. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    let imageUrl: String
+    let name: String
+    let description: String
+    let voteCount: Int
+}
---
+++ Wongnai-IOS-Assignment/ViewModels/PhotoListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoListViewModel.swift
+//  Wongnai-IOS-Assignment
+//
+//  Created by Natthawut Kaeoaubon on 16/8/2563 BE.
+//  Copyright © 2563 Natthawut Kaeoaubon. All rights reserved.
+//
+
+import Foundation
+
+class PhotoListViewModel {
+    
+}
