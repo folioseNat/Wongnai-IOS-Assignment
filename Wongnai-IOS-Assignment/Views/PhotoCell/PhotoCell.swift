e8b71f694dbfcf4b61e4634e8d95e8c7e080e2d5
Natthawut.K <foliose.k.natthawut@gmail.com>
2020-08-16 13:20:59 +0700

    create custom photo cell

---
+++ Wongnai-IOS-Assignment/Views/PhotoCell/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Wongnai-IOS-Assignment
+//
+//  Created by Natthawut Kaeoaubon on 16/8/2563 BE.
+//  Copyright © 2563 Natthawut Kaeoaubon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var votesCountLabel: UILabel!
+    @IBOutlet weak var photoDescription: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
