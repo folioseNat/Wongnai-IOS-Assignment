ea8235e6e36705d9edb791ac6e62abd2b211725f
Natthawut.K <foliose.k.natthawut@gmail.com>
2020-08-17 08:54:12 +0700

    create func to load data using alamofire

--- Wongnai-IOS-Assignment/ViewModels/PhotoListViewModel.swift
+++ Wongnai-IOS-Assignment/ViewModels/PhotoListViewModel.swift
@@ -6,8 +6,52 @@
 //  Copyright © 2563 Natthawut Kaeoaubon. All rights reserved.
 //
 
-import Foundation
+
+import RxSwift
+import RxCocoa
+import Alamofire
 
 class PhotoListViewModel {
+    let publishPhotos = BehaviorRelay<[Photo]>(value: [])
+    
+    func loadPhoto(isInit: Bool = true, page: Int) {
+        AF.request("https://api.500px.com/v1/photos?feature=popular&page=\(page)")
+            .validate(contentType: ["application/json"])
+            .responseJSON { (response) in
+                switch response.result {
+                case let .success(result):
+                    if let data = result as? NSDictionary, let photo = data["photos"] as? [NSDictionary] {
+                        
+                        let photoArray = photo.map { (result) -> Photo in
+                            return self.mapJsonToPhoto(json: result)
+                        }
+                        if isInit {
+                            self.publishPhotos.accept(photoArray)
+                        } else {
+                            var newPhotos = self.publishPhotos.value
+                            newPhotos.append(contentsOf: photoArray)
+                            self.publishPhotos.accept(newPhotos)
+                        }
+                    }
+                    
+                case let .failure(error):
+                    print(error.localizedDescription)
+                }
+        }
+    }
+    
+    func mapJsonToPhoto(json: NSDictionary) -> Photo {
+        if let imageUrl = json["image_url"] as? [String],
+            let name = json["name"] as? String,
+            let description = json["description"] as? String,
+            let voteCount = json["votes_count"] as? Int  {
+            
+            return Photo(imageUrl: imageUrl.first ?? "", name: name, description: description, voteCount: voteCount)
+            
+        } else {
+            return Photo(imageUrl: "", name: "", description: "", voteCount: 0)
+        }
+    }
+    
     
 }
