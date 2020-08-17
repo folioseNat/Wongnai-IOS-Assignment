bb2c0542dd7cbe789831662991e042a1faa6f3c8
Natthawut.K <foliose.k.natthawut@gmail.com>
2020-08-17 08:46:05 +0700

    add image insertion cell

---
+++ Wongnai-IOS-Assignment/Views/ImageInsertionCell/ImageInsertionCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageInsertionCell.swift
+//  Wongnai-IOS-Assignment
+//
+//  Created by Natthawut Kaeoaubon on 16/8/2563 BE.
+//  Copyright © 2563 Natthawut Kaeoaubon. All rights reserved.
+//
+
+import UIKit
+
+class ImageInsertionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
